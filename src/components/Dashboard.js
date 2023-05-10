156a4085795540a5a3b01771fbcaa36e112f810d
Phillip Chae <msc694@nyu.edu>
2023-05-10 22:22:14 +0900

    Try to make the app work in static servers as well

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,14 +1,12 @@
 // Importing necessary libraries and components
 import React, { useState, useEffect } from "react";
-import axios from 'axios';
 import Button from "./Button";
 import "./Dashboard.css";
-const { apiUrl } = require('../config');
 
 // Defining Dashboard component
 const Dashboard = () => {
 
-  // Declaring state variables for button list, adding new button form, new button name, and new button URLs
+  // Declaring state variables for button list, adding new button form, new button name, new button URLs
   const [buttons, setButtons] = useState([]);
   const [showAddButtonForm, setShowAddButtonForm] = useState(false);
   const [newButtonName, setNewButtonName] = useState("");
@@ -53,26 +51,18 @@ const Dashboard = () => {
 
   // Function to save button data to the API
   const saveButtons = () => {
-    axios.post(`${apiUrl}/write-json/buttons`, buttons)
-      .then(response => {
-        console.log(response.data);
-      })
-      .catch(error => {
-        console.error(error);
-      });
+    localStorage.setItem("buttons", JSON.stringify(buttons));
   };
 
+  const loadButtons = () => {
+    const storedButtons = localStorage.getItem("buttons");
+    if (storedButtons) {
+      setButtons(JSON.parse(storedButtons));
+    }
+  }
   // useEffect hook to fetch button data from API on component mount
   useEffect(() => {
-    axios.get(`${apiUrl}/read-json/buttons`)
-      .then(response =>{
-        const data = response.data;
-        setButtons(data);
-        console.log(data);
-      })
-      .catch(error => {
-        console.error(error);
-      })
+    loadButtons();
   }, []);
   
   //bugFixer is made so that the saveButtons() is not run before the json file is read
